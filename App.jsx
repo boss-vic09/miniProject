7a7511fd0bdc63c4770f30b54d1ebbe7c9e885b3
boss-vic09 <167195791+boss-vic09@users.noreply.github.com>
2025-04-16 10:54:33 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,41 @@
-import './App.css'
+import './App.css';
+import {useState} from 'react';
 
 function App() {
+ const [count, setCount] = useState(0);
+ const [message, setMessage] = useState('');   // message to show errors or limit notices
+
+ 
+ //Event handler for the decrement function 
+ function decrement(){
+   if (count <= 0) {
+      setMessage("Can't go below 00"); // Show message if limit reached
+    } else {
+      setCount(count - 1);               // decrease the count
+      setMessage('');                    // Clear any previous message
+    }
+  };
+  
+  //Event handler for the increasing function
+  function increment(){
+    if (count >= 1000) {
+      setMessage("Can't go above 1000"); // Show message if limit reached
+    } else {
+      setCount(count + 1);               // Increase the count
+      setMessage('');                    // Clear any previous message
+    }
+  };
   
   return (
     <>
-      <div>
-        <p>Hello world!</p>
+      <h1>Click Counter App</h1>
+      <div className = "container">
+        <h2> {count} </h2>
       </div>
+       {/* Display message only if it's not empty */}
+      {message && <p>{message}</p>}
+      <Button text = "Add up" onClick = {increment} />
+      <Button text = "Subtract" onClick = {decrement} />
     </>
   )
 }
